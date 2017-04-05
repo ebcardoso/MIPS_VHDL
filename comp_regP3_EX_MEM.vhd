d30072a1b72e2d0a37b66b34a88b4e4df96fbe8a
Breno Cardoso <elisiobreno@gmail.com>
2017-04-04 22:45:16 -0300

    Sinais de Controle nos Registradores dos Estágios

--- comp_regP3_EX_MEM.vhd
+++ comp_regP3_EX_MEM.vhd
@@ -10,10 +10,32 @@ entity comp_regP3_EX_MEM is
 		new_sum  : in  STD_LOGIC_VECTOR(31 DOWNTO 0);
 		new_D2   : in  STD_LOGIC_VECTOR(31 DOWNTO 0);
 		
+		--sinais de controle entrada		
+		--MEM
+		new_Branch     : in STD_LOGIC;
+		new_LeMem      : in STD_LOGIC;
+		new_EscreveMem : in STD_LOGIC;
+		
+		--WB
+		new_EscreveReg : in STD_LOGIC;
+		new_MemparaReg : in STD_LOGIC;
+		--
+		
 		Q_zero : out STD_LOGIC;
 		Q_ula  : out STD_LOGIC_VECTOR(31 DOWNTO 0);
 		Q_sum  : out STD_LOGIC_VECTOR(31 DOWNTO 0);
-		Q_D2   : out STD_LOGIC_VECTOR(31 DOWNTO 0)
+		Q_D2   : out STD_LOGIC_VECTOR(31 DOWNTO 0);
+		
+		--sinais de controle saida		
+		--MEM
+		Q_Branch     : out STD_LOGIC;
+		Q_LeMem      : out STD_LOGIC;
+		Q_EscreveMem : out STD_LOGIC;
+		
+		--WB
+		Q_EscreveReg : out STD_LOGIC;
+		Q_MemparaReg : out STD_LOGIC
+		--
 	);
 end comp_regP3_EX_MEM;
 
@@ -22,10 +44,24 @@ architecture arc of comp_regP3_EX_MEM is
 	signal reg_ula  : STD_LOGIC_VECTOR(31 DOWNTO 0) := "00000000000000000000000000000000";
 	signal reg_sum  : STD_LOGIC_VECTOR(31 DOWNTO 0) := "00000000000000000000000000000000";
 	signal reg_D2   : STD_LOGIC_VECTOR(31 DOWNTO 0) := "00000000000000000000000000000000";
+	
+	--registradores dos sinais de controle
+	--MEM
+	signal reg_Branch     : STD_LOGIC := '0';
+	signal reg_LeMem      : STD_LOGIC := '0';
+	signal reg_EscreveMem : STD_LOGIC := '0';
+	
+	--WB
+	signal reg_EscreveReg : STD_LOGIC := '0';
+	signal reg_MemparaReg : STD_LOGIC := '0';
 begin
 	process (clk,
 				reg_zero, reg_ula, reg_sum, reg_D2,
-				new_zero, new_ula, new_sum, new_D2)
+				new_zero, new_ula, new_sum, new_D2,
+				new_Branch, new_LeMem, new_EscreveMem,
+				new_EscreveReg, new_MemparaReg,
+				reg_Branch, reg_LeMem, reg_EscreveMem,
+				reg_EscreveReg, reg_MemparaReg)
 	begin
 		if (clk = '1' and clk'event) then
 			Q_zero <= reg_zero;
@@ -33,10 +69,26 @@ begin
 			Q_sum  <= reg_sum;
 			Q_D2   <= reg_D2;
 			
+			--MEM
+			Q_Branch     <= reg_Branch;
+			Q_LeMem      <= reg_LeMem;
+			Q_EscreveMem <= reg_EscreveMem;
+			--WB
+			Q_EscreveReg <= reg_EscreveReg;
+			Q_MemparaReg <= reg_MemparaReg;
+			
 			reg_zero <= new_zero;
 			reg_ula  <= new_ula;
 			reg_sum  <= new_sum;
 			reg_D2   <= new_D2;
+			
+			--MEM
+			reg_Branch     <= new_Branch;
+			reg_LeMem      <= new_LeMem;
+			reg_EscreveMem <= new_EscreveMem;	
+			--WB
+			reg_EscreveReg <= new_EscreveReg;
+			reg_MemparaReg <= new_MemparaReg;	
 		end if;
 	end process;
 end arc;
--- comp_regP4_MEM_WB.vhd
+++ comp_regP4_MEM_WB.vhd
@@ -9,22 +9,49 @@ entity comp_regP4_MEM_WB is
 		new_dado_leitura  : in  STD_LOGIC_VECTOR(31 DOWNTO 0);
 		
 		Q_ula          : out STD_LOGIC_VECTOR(31 DOWNTO 0);
-		Q_dado_leitura : out STD_LOGIC_VECTOR(31 DOWNTO 0)
+		Q_dado_leitura : out STD_LOGIC_VECTOR(31 DOWNTO 0);
+		
+		--sinais de controle entrada			
+		--WB
+		new_EscreveReg : in STD_LOGIC;
+		new_MemparaReg : in STD_LOGIC;
+		--
+		
+		--sinais de controle saida		
+		--WB
+		Q_EscreveReg : out STD_LOGIC;
+		Q_MemparaReg : out STD_LOGIC
+		--
 	);
 end comp_regP4_MEM_WB;
 
 architecture arc of comp_regP4_MEM_WB is
 	signal reg_ula          : STD_LOGIC_VECTOR(31 DOWNTO 0) := "00000000000000000000000000000000";
 	signal reg_dado_leitura : STD_LOGIC_VECTOR(31 DOWNTO 0) := "00000000000000000000000000000000";
+	
+	--registradores dos sinais de controle
+	--WB
+	signal reg_EscreveReg : STD_LOGIC := '0';
+	signal reg_MemparaReg : STD_LOGIC := '0';
 begin
-	process (clk, reg_ula, reg_dado_leitura, new_ula, new_dado_leitura)
+	process (clk, reg_ula, reg_dado_leitura, new_ula, new_dado_leitura,
+				new_EscreveReg, new_MemparaReg,
+				reg_EscreveReg, reg_MemparaReg)
 	begin
 		if (clk = '1' and clk'event) then
 			Q_ula           <= reg_ula;
 			Q_dado_leitura  <= reg_dado_leitura;
 			
+			--WB
+			Q_EscreveReg <= reg_EscreveReg;
+			Q_MemparaReg <= reg_MemparaReg;
+			
 			reg_ula          <= new_ula;
 			reg_dado_leitura <= new_dado_leitura;
+			
+			--WB
+			reg_EscreveReg <= new_EscreveReg;
+			reg_MemparaReg <= new_MemparaReg;
 		end if;
 	end process;
 end arc;
