1dc1ed8a1cd1411ae1bae57586d59e238935c3a7
Breno Cardoso <elisiobreno@gmail.com>
2017-08-16 23:04:29 -0300

    sll, srl, slt no controle da ULA

--- comp_ULA_Controle.vhd
+++ comp_ULA_Controle.vhd
@@ -37,7 +37,9 @@ BEGIN
 			elsif (funct = "100111") then --ori
 				out0 <= "0001";
 			elsif (funct = "101000") then --sll
+				out0 <= "1110";
 			elsif (funct = "101001") then --srl
+				out0 <= "1111";
 			elsif (funct = "101010") then --slt -- livro
 				out0 <= "0111";
 			elsif (funct = "101011") then --slti
